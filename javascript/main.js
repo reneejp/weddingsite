69afb277aaf917ca7f622437552f1605f18b9a5f
Renee Perron <reneejperron@gmail.com>
2015-10-02 15:49:06 -0400

    Adding instagram plugin and updating file structure

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,18 @@
-(function(){ 
-  $('.carousel').slick({
-    dots: true,
+
+  // slick carousel loader
+  $('.carousel-slick').slick({
+  	arrows: true,
   	infinite: true,
   	speed: 500,
   	fade: true,
   	cssEase: 'linear',
-  	autoplay: true,
+  	autoplay: false,
   });
 
-
-
-});
+ 
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'johnandrenee2015',
+        clientId: '1e7f72a528b649d5b9d0ef17c5e3855a'
+       });
+      feed.run();
